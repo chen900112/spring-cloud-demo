0559ff06b0dc62c311addab33234875802379457
chen9001 <236514977@qq.com>
2023-04-01 08:04:35 +0800

    nacons 配置中心

--- stock-service/src/main/java/com/springclouddemo/stock/Application.java
+++ stock-service/src/main/java/com/springclouddemo/stock/Application.java
@@ -3,12 +3,24 @@ package com.springclouddemo.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 
 @SpringBootApplication
 public class Application
 {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(Application.class, args);
+    public static void main( String[] args ) throws InterruptedException {
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+        while (true){
+            String username = run.getEnvironment().getProperty("user.name");
+            String userage = run.getEnvironment().getProperty("user.age");
+            String usercfg = run.getEnvironment().getProperty("user.cfg");
+            System.out.println("name:" + username+"; age:"+ userage+"; cfg:"+ usercfg);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
     }
 }
